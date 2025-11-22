18e2fb2a40100f0c9892a30713b9f33db119d2a7
Cereal_Dev <ajedavid1@gmail.com>
2025-11-22 17:54:14 +0000

    solved question 5 through normal methods

---
+++ Problem-5/p5.py
@@ -0,0 +1,6 @@
+
+# I took a normal non computational approach to solving this question 
+# through finding the lcm of numbers from 1 to 20
+# getting an answer of 232792560
+# The method
+# i got all te prime factors and then 
